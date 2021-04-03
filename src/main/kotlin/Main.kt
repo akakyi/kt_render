2b6de512bf34b4a8cbc2d0774312498c90b4fcbf
Fomin <mikhail.fomin@simbirsoft.com>
2021-04-03 09:53:13 +0400

    render green square

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+import kotlinx.browser.document
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+
 fun main() {
-    console.log("Test!")
+    console.log("Test ebtb!")
+    val mainWindow = document.getElementById("main_window") as HTMLCanvasElement
+
+    var ctx = mainWindow.getContext("2d") as CanvasRenderingContext2D
+    ctx.fillStyle = "green"
+    ctx.fillRect(10.0, 10.0, 100.0, 100.0)
 }
