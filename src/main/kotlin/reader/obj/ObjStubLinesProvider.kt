2339dec0184fee26439fd9994d79a04349cfcec8
Fomin Mikhail <m.fomin@elama.ru>
2022-12-05 00:44:54 +0400

    minor changes

--- src/main/kotlin/reader/obj/ObjStubLinesProvider.kt
+++ src/main/kotlin/reader/obj/ObjStubLinesProvider.kt
@@ -2,6 +2,6 @@ package reader.obj
 
 class ObjStubLinesProvider : ObjLinesProvider {
 
-    override fun forEachLine(handler: (line: String) -> Unit) = LINES.forEach { handler(it) }
+    override fun forEachLine(handler: (line: String) -> Unit) = AFRICAN_MAN_LINES.forEach { handler(it) }
 
 }
